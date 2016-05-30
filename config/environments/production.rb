392ed1d4ff640e7630f22412fbc51a75bc1ef8b0
Daniel Moros <daniel@daniel.desarrollo>
2016-05-29 22:07:21 -0400

    Ajuste de production.rb rollback

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,6 @@
 Aceim::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-
-  config.assets.compile = true
-  config.assets.initialize_on_precompile = false
-
-
   # Code is not reloaded between requests
   config.cache_classes = true
 
@@ -14,7 +9,7 @@ Aceim::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
