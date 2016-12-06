8a696bcd95aa437b139d83a895774be75f283b38
bursondevelopment <burson.ve.de@gmail.com>
2016-12-06 11:00:17 -0400

    Nuevo ajuste de TimeZone config en aplication.rb

--- config/application.rb
+++ config/application.rb
@@ -29,8 +29,8 @@ module Aceim
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Caracas'
-
+    config.time_zone = 'Caracas'
+    #config.active_record.default_timezone = 'Caracas'
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
