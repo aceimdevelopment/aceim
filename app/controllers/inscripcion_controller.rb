e4c6ecd72de517b65926cfa6fecbb8c6e98c26eb
Daniel Moros <aceim.development@gmail.com>
2020-07-27 10:18:57 -0400

    Ajuste

--- app/controllers/inscripcion_controller.rb
+++ app/controllers/inscripcion_controller.rb
@@ -12,7 +12,7 @@ class InscripcionController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @historial_academico }
+      format.json { json: @historial_academico}
     end    
   end
 
